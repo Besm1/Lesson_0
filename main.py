667b205f175e6e3fea058b65ae43f0cced1f33f2
Besm1 <besm1@yandex.ru>
2024-07-17 23:45:01 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+# 1st program -- Арифметика
+print(9 ** 0.5 * 5)
+print(5 * 9 ** 0.5) # Exponentiation operation has priority over multiplication!
+
+# 2nd program -- Сравнение, or, and
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program -- "Сложная арифметика"
+print(1234 // 10 % 100 + 5678 // 10 % 100)
+
+# 4th program -- "Всё, везде и сразу"
+print(int(42.13) == 13.42 * 100 % 100 or int(13.42) == 42.13 * 100 % 100)
